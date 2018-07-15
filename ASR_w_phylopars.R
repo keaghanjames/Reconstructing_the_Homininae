eac14bd94c9a2bf71fb68f73e901fd65118f3ce9
keaghanjames <keaghan.yaxley@gmail.com>
2018-07-15 15:27:46 +0100

    Update and rename new_script.R to ASR_w_phylopars.R

--- ASR_w_phylopars.R
+++ ASR_w_phylopars.R
@@ -1,3 +1,5 @@
+#the following script executes the procedure described in section 3.4.2.
+
 #load in requisite packages
 require(RColorBrewer)
 require(ape)
@@ -15,71 +17,63 @@ deep.tree <- read.nexus("min.gamma.tree")
 shallow.tree <- read.nexus("max.gamma.tree")
 trees <- c(mcc.tree, old.tree, young.tree, deep.tree, shallow.tree)
 treenames <- c("mcc.tree.Rdata", "old.tree.Rdata", "young.tree.Rdata", "deep.tree.Rdata", "shallow.tree.Rdata")
-#homtree <- mcc.tree
-#set teh director to were you want the experimen to run
-setwd("~/Desktop/Analyses/Non_Cultural_Traits/body_size_integrated/")
+#set the directory to wherever you want the experiment to run from - wherever your trait data is stored
+setwd("~/Desktop/Analyses/Non_Cultural_Traits/body_size_male/")
 output <- vector()#a vector into which we will place the reulst of each analysis
 
+#we begin the loop - it repeates the analysis across of all the trees included in trees - theoretically this could be
+#used to analyse many more trees however here we limit the analysis to the five selected
 for (i in 1:length(trees)) {
   tryCatch({
-#read in the trait data - need to change according to the csv
-  traits <- read.csv("body_size_integrated.csv")
-  traits$X <- NULL #just for the integrated run
+#read in the trait data - this will need to change dependening on the reconstructed trait
+  traits <- read.csv("body_size_male.csv")
 homtree <- trees[[i]]
 numbers <- vector()#a vector to which the results of interest will be placed
-#rearrange the rows so they are in the same order and have exactly the same names as the tips
-#rownames(traits) <- traits$species
-#traits <- traits[homtree$tip.label,]
-#rownames(traits) <- homtree$tip.label #rename them all because a few get lost for not apparent reason
-#traits$species <- homtree$tip.label
 
 #having set up the data frame we now log-transform the trait values
-traits[2:3] <- log(traits[2:3])
+traits[2:3] <- log(traits[2:3]) #do not do this when using the cultural diversity index, these are already log values
 
 #next we check whether the trait actually shows any phylogenetic signal
 #first calculate Pagel's lambda for the trait
 lambda <- phylopars(traits, homtree, model = "lambda")
-numbers <- c(numbers, lambda$model$lambda)
-numbers <- c(numbers, lambda$logLik)
-#SAVE LAMBDA
-#then we model the traits but assume a star phylogeny - this acts as a null
+numbers <- c(numbers, lambda$model$lambda)#save the value of lambda
+numbers <- c(numbers, lambda$logLik)#save the log-likelihood
+
+#then we model the traits but assume a star phylogeny - this will act as our null
 star <- phylopars(traits, homtree, model = "star")
-numbers <- c(numbers, star$logLik)
-#SAVE STAR
-#now we can do a log-likelihood to test the two models
-chi_square <- as.double(2*(logLik(lambda) - # 2*(logLik_alt - logLik_null)
-                             logLik(star))) 
-numbers <- c(numbers, chi_square)
+numbers <- c(numbers, star$logLik) #again we save the log-likelihood
+
+#now we can do a log-likelihood ratio test using the two models
+chi_square <- as.double(2*(logLik(lambda) - logLik(star))) # 2*(logLik_alt - logLik_null)
+                             
+numbers <- c(numbers, chi_square)# save the chi_square values
 degrees_freedom <- lambda$npars - star$npars # df = difference in # model parameters
-numbers <- c(numbers, degrees_freedom)
-p_value <- pchisq(q = chi_square,df = degrees_freedom,lower.tail = FALSE) # p-value
+numbers <- c(numbers, degrees_freedom) #save the degress of freedom
+p_value <- pchisq(q = chi_square,df = degrees_freedom,lower.tail = FALSE) # save the p-value
 numbers <- c(numbers, p_value)
-print(p_value)
 
 #reconstrcut under various phylogenetic models
-BM <- phylopars(trait_data = traits, homtree, model = "BM")
+BM <- phylopars(trait_data = traits, homtree, model = "BM") #the Brownian motion model
 #save(BM, file = "BM.Rdata")
-mvOU <- phylopars(traits, homtree, model = "mvOU")
-#save(mvOU, file = "mvOU.Rdata")
-OU <- phylopars(traits, homtree, model = "OU")
+OU <- phylopars(traits, homtree, model = "OU") #the Ornstein-Uhlenbeck model
 #save(OU, file = "OU.Rdata")
-EB <- phylopars(traits, homtree, model = "EB")
+EB <- phylopars(traits, homtree, model = "EB")# the early burst model
 #save(EB, file = "EB.Rdata")
 
 #combine outputs into single list
-models <- list(BM, mvOU, OU, EB)
+models <- list(BM, OU, EB)
 #get AIC for each of the models
-best_model <- c(AIC(BM), AIC(mvOU), AIC(OU), AIC(EB))
+best_model <- c(AIC(BM), AIC(OU), AIC(EB))
 
 #now identify the model with the lowest AIC
 x <- which.min(best_model)
-numbers <- c(numbers, best_model[[x]])
-best_model <- models[[x]]
-numbers <- c(numbers, best_model$model$model, best_model$mu, best_model$npars)
-#print(best_model)
-save(best_model, file = paste(treenames[i]))
-#now we view the imputed missing data and the imputation variance 
-node_states <- best_model$anc_recon # Data with imputed species means
+numbers <- c(numbers, best_model[[x]]) #record which model it is in numbers
+best_model <- models[[x]] #select that model
+numbers <- c(numbers, best_model$model$model, best_model$mu, best_model$npars) #save its most important parameters 
+save(best_model, file = paste(treenames[i]))#save the data from this model incase we need it later
+
+#now we extract the ancestral state estimates and the variance
+node_states <- best_model$anc_recon # Data with ancestral state estimates
 node_variance <- best_model$anc_var # Variances for each estimate
 
 #and compute 95%CI by using the square root
@@ -89,10 +83,10 @@ upperCI <- best_model$anc_recon + sqrt(best_model$anc_var)*1.96 # Upper 95% CI
 #if you log transformed the traits you'll want to reverse that before you make the figures
 node_states <- exp(node_states) #convert back to meaningful values
 node_variance <- exp(node_variance) #convert back to meaningful value
-lowerCI <- exp(lowerCI)
-upperCI <- exp(upperCI)
+lowerCI <- exp(lowerCI)#convert back to meaningful value
+upperCI <- exp(upperCI)#convert back to meaningful value
 
-#to make some figues we first need to organise the tip and node values into matrices
+#to make some figures we first need to organise the tip and node values into matrices
 node_states <- as.matrix(node_states)[,1]
 tip_states <- node_states[1:length(homtree$tip.label)]
 
@@ -101,12 +95,14 @@ names(tips.ancestors) <- NULL
 lower <- as.vector(lowerCI)
 upper<- as.vector(upperCI)
 tips.ancestors <- as.vector(rbind(tips.ancestors, lower, upper))
+
+#and finally we place our ancestral state estimates into numbers
 numbers <- c(numbers, tips.ancestors)
 #now we make a phenogram
 fancyTree(homtree, type = "phenogram95", x = tip_states)
 phenogram(homtree, node_states, add = T, ftype = "off")
 
-#and we make that phylogeny with the coloured nodes
+#and we make  phylogeny with the coloured nodes
 tips_states_cut <- cut(node_states[1:length(homtree$tip.label)], breaks = 5, labels = F)
 node_states_cut <- cut(node_states[length(homtree$tip.label)+1:length(node_states)], breaks = 5, labels = F)
 plot(homtree, type="p", use.edge.length = TRUE, label.offset=1,cex=0.6) 
@@ -115,10 +111,10 @@ nodelabels(pch=21,bg=CP[node_states_cut],col="black",cex=2,adj=0.505)
 op<-par(xpd=TRUE)
 legend(0,0, legend=levels(cut(node_states, breaks=5)),
        col=CP,pch=20,bty="n",cex=0.7,pt.cex=1.5,ncol=5)
-  }, error=function(e){})
-  output <- rbind(output, numbers)
+  }, error=function(e){}) #this ensures that the loop continues even if it encounters an error
+  output <- rbind(output, numbers) #place numbers in output
 }
-rownames(output) <- c("mcc.tree", "old.tree", "young.tree", "deep.tree", "shallow.tree")
+rownames(output) <- c("mcc.tree", "old.tree", "young.tree", "deep.tree", "shallow.tree") #name the rows by tree
 thestats <- c("lambda", "lambda_loglik", "star_loglik", "chi_square", "df", "p_value", "AIC", "Model", "mu", "npars",
                       "Gorilla_beringei_graueri", "lower95", "upper95", "Gorilla_beringei_beringei", "lower95", "upper95", 
                       "Gorilla_gorilla_diehli", "lower95", "upper95", "Gorilla_gorilla_gorilla",  "lower95", "upper95",
@@ -128,7 +124,6 @@ thestats <- c("lambda", "lambda_loglik", "star_loglik", "chi_square", "df", "p_v
                       "upper95", "root", "lower95", "upper95", "LCAGHP", "lower95", "upper95", "LCAG", "lower95", "upper95", 
                       "LCAGb", "lower95", "upper95", "LCAGg", "lower95", "upper95", "LCAPH", "lower95", "upper95", "LCAP", 
                       "lower95", "upper95", "LCAPt", "lower95", "upper95", "LCAPtve", "lower95", "upper95", "LCAPtts", "lower95", 
-                      "upper95")
-colnames(output) <- thestats
-write.csv(output, file = "output.csv")
-
+                      "upper95") #name the columns - there's a lot of them. 
+colnames(output) <- thestats #give the columns their names
+write.csv(output, file = "output.csv") #save the output of the ancestral state reconstruction
